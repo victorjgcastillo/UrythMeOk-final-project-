8c5e1331ef51eb0d24dd2e3f6c6f988b8764f76f
José Miguel Madueño Jurado <josemi.98@hotmail.es>
2023-02-17 11:10:44 +0100

    galeria añadida

--- FrontEnd/rithme/src/pages/HomePage.jsx
+++ FrontEnd/rithme/src/pages/HomePage.jsx
@@ -1,14 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
 import './HomePage.scss';
 
 export default function HomePage() {
+    
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        axios('http://localhost:5000/artists')
+        .then(res => {
+            console.log(res.data);
+            setCategories(res.data)
+        })
+    }, [])
+
     return (
 
         <div className="home"> 
         <Header></Header>
-        <button className="rounded-md ...">Conciertos</button>
-        <button className="rounded-full">Amigos</button>
-        <button className="rounded-full">Fan Club</button>
+        <button className="rounded-full home__button">Conciertos</button>
+        <button className="rounded-full home__button">Amigos</button>
+        <button className="rounded-full home__button">Fan Club</button>
 
 
             <div className="home__categories">
@@ -19,7 +32,22 @@ export default function HomePage() {
                 </div>
 
                 <div className="home__categories--types">
+
                     <h3 class="font-bold">ARTISTAS</h3>
+                    
+                        <ul className="homes__categories--ul">
+
+                        {categories.map((categorie, id)=> (
+                        <figure key={id}>
+
+                                <img src={categorie.img} alt={categorie.name} className="home__categories--img"></img>   
+                                <h1 className="homes__escudos--name">{categorie.name}</h1>         
+
+                        </figure>
+                        ))}
+                        </ul>
+
+
                     <h3 class="font-bold">ESTILOS</h3>
                     <h3 class="font-bold">SALAS</h3>
                     <h3 class="font-bold">ONDAS</h3>
