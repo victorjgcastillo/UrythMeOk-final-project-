4a3073b63d60e89c6d2baa935dcf0475a9721c9c
José Miguel Madueño Jurado <josemi.98@hotmail.es>
2023-02-17 13:53:37 +0100

    slider in progress

--- FrontEnd/rithme/src/pages/HomePage.jsx
+++ FrontEnd/rithme/src/pages/HomePage.jsx
@@ -1,19 +1,20 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+// import axios from "axios";
+// import { useEffect, useState } from "react";
 import Header from "../components/Header/Header";
+import Slider from "../components/Slider/Slider";
 import './HomePage.scss';
 
 export default function HomePage() {
     
-    const [categories, setCategories] = useState([]);
+    // const [categories, setCategories] = useState([]);
 
-    useEffect(() => {
-        axios('http://localhost:5000/artists')
-        .then(res => {
-            console.log(res.data);
-            setCategories(res.data)
-        })
-    }, [])
+    // useEffect(() => {
+    //     axios('http://localhost:5000/artists')
+    //     .then(res => {
+    //         console.log(res.data);
+    //         setCategories(res.data)
+    //     })
+    // }, [])
 
     return (
 
@@ -24,34 +25,45 @@ export default function HomePage() {
         <button className="rounded-full home__button">Fan Club</button>
 
 
-            <div className="home__categories">
 
-                <div className="home__categories--title">
+                <div className="home__title">
                     <h1 className="text-5xl">¡Hola Mundo!</h1>
                     <h2 className="text-2xl">¿Qué concierto te apetece?</h2>
                 </div>
 
-                <div className="home__categories--types">
 
-                    <h3 class="font-bold">ARTISTAS</h3>
+                <div className="home__categories">
+
+                <div className="home__categories--section">
+
+                    <h3 className="font-bold home__categories--section--artist">ARTISTAS</h3>
+
+                    <Slider></Slider>
                     
-                        <ul className="homes__categories--ul">
+                        {/* <ul className="homes__categories--section--ul">
 
                         {categories.map((categorie, id)=> (
                         <figure key={id}>
 
-                                <img src={categorie.img} alt={categorie.name} className="home__categories--img"></img>   
-                                <h1 className="homes__escudos--name">{categorie.name}</h1>         
+                                <img src={categorie.img} alt={categorie.name} className="home__categories--section--ul--img"></img>   
 
                         </figure>
                         ))}
-                        </ul>
+                        </ul> */}
+
+                        {/* <div className="home__categories--section--artist--carrousel">
+                            <div className="home__categories--section--artist-carrousel--slides">
+                                {categories.map(categorie => (
+                                <img key={categorie.id} src={categorie.img} alt={categorie.name} className="home__categories--section--artist-carrousel--slides--img"/>
+                                ))}
+                            </div>
+                        </div> */}
 
 
-                    <h3 class="font-bold">ESTILOS</h3>
-                    <h3 class="font-bold">SALAS</h3>
-                    <h3 class="font-bold">ONDAS</h3>
-                    <h3 class="font-bold">REVISTAS</h3>
+                    <h3 className="font-bold">ESTILOS</h3>
+                    <h3 className="font-bold">SALAS</h3>
+                    <h3 className="font-bold">ONDAS</h3>
+                    <h3 className="font-bold">REVISTAS</h3>
                 </div>
 
 
